421fe974475e50a1130a7c2be99dc7d9c2bc6e0d
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-04-16 15:50:51 +0600

    add model

--- RockPaperScissorsApp/RockPaperScissorsApp/ContentView.swift
+++ RockPaperScissorsApp/RockPaperScissorsApp/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     @State private var isPlaying = false
     
     var body: some View {
-        //        Result(title: "Tie!", color: [Color(red: 255/255, green: 204/255, blue: 0/255), Color(red: 255/255, green: 92/255, blue: 0/255)])
         NavigationView {
             VStack {
                 WelcomePage(isPlaying: $isPlaying)
@@ -47,13 +46,26 @@ struct WelcomePage: View {
     }
 }
 
+struct headerText: View {
+    @Binding var color: [Color]
+    @Binding var text: String
+    var body: some View {
+        Text(text)
+            .gradientForeground(colors: color)
+            .font(.system(size: 54))
+            .multilineTextAlignment(.center)
+            .fontWeight(.bold)
+    }
+}
+
 struct takeYourPickSingle: View {
     var playerPick = "Your pick"
     var takeYourPick = "Take your pick"
     var thinking = "Your\n opponent is\n thinking"
     var oponentPick = "Your opponent’s pick"
-    @State private var buttonWidth = 348
     @State private var header = "Take your pick"
+    @State private var loadOrPick = "loading"
+    @State private var result = "Tie!"
     @State private var changed = false
     @State private var selectedPaper = false
     @State private var selectedScissors = false
@@ -61,17 +73,22 @@ struct takeYourPickSingle: View {
     @State private var selected = false
     @State private var next = false
     @State private var showScore = true
-    @State private var loadOrPick = "loading"
+    @State private var buttonWidth = 348
+    @State private var offsetX1: CGFloat = 0
+    @State private var offsetY1: CGFloat = 0
+    @State private var offsetX2: CGFloat = UIScreen.main.bounds.width
+    @State private var offsetY2: CGFloat = 0
+    @State private var color: [Color] = [.black]
+    @State private var anotherRound = false
+    var loseColor = [Color(red: 255/255, green: 105/255, blue: 97/255), Color(red: 253/255, green: 77/255, blue: 77/255)]
+    var winColor = [Color(red: 181/255, green: 238/255, blue: 155/255), Color(red: 36/255, green: 174/255, blue: 67/255)]
+    var tieColor = [Color(red: 255/255, green: 204/255, blue: 0/255), Color(red: 255/255, green: 92/255, blue: 0/255)]
     
     var body: some View {
         ZStack {
             VStack(spacing: 74) {
                 VStack(spacing: 12) {
-                    Text(header)
-                        .foregroundColor(.black)
-                        .font(.system(size: 54))
-                        .multilineTextAlignment(.center)
-                        .fontWeight(.bold)
+                    headerText(color: $color, text: $header)
                     
                     if showScore {
                         Text("Score 0:0")
@@ -95,7 +112,7 @@ struct takeYourPickSingle: View {
                     
                     if next {
                         ZStack {
-                            loadOrPickImage(imageName: $loadOrPick)
+                            loadOrPickImage(imageName: $loadOrPick, offsetX: $offsetX1, offsetY: $offsetY1)
                                 .onAppear {
                                     if loadOrPick == "loading" {
                                         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
@@ -104,12 +121,28 @@ struct takeYourPickSingle: View {
                                                 loadOrPick = "paper"
                                             }
                                         }
+                                        
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
+                                            withAnimation(.spring()) {
+                                                header = ""
+                                                offsetY1 = -80
+                                                offsetX1 = -70
+                                            }
+                                        }
+                                        
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 6) {
+                                            withAnimation(.spring()) {
+                                                header = result
+                                                showScore = true
+                                                anotherRound = true
+                                                color = loseColor
+                                            }
+                                        }
                                     }
                                 }
-//                                .offset(x: -50)
                                 .padding(.bottom, 55)
                             
-                            loadOrPickImage(imageName: $loadOrPick)
+                            loadOrPickImage(imageName: $loadOrPick, offsetX: $offsetX2, offsetY: $offsetY2)
                                 .onAppear {
                                     if loadOrPick == "loading" {
                                         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
@@ -118,10 +151,25 @@ struct takeYourPickSingle: View {
                                                 loadOrPick = "paper"
                                             }
                                         }
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                                            withAnimation(.spring()) {
+                                                offsetY2 = -10
+                                                offsetX2 = 70
+                                            }
+                                        }
                                     }
                                 }
                                 .padding(.bottom, 55)
-//                                .offset(x: -70, y: -70)
+                            
+                            if anotherRound {
+                                withAnimation {
+                                    VStack {
+                                        Spacer()
+                                        changeOrNext(changeView: $changed, text: "Another round")
+                                    }
+                                    .padding(.top, 500)
+                                }
+                            }
                         }
                     }
                 }
@@ -182,17 +230,14 @@ struct takeYourPickSingle: View {
             }
         }
     }
-    
-    var result: some View {
-        Text("hello world")
-    }
+   
 }
 
 struct loadOrPickImage: View {
     @Binding var imageName: String
     @State var Width: CGFloat = 348
-    @State var offsetX: CGFloat = 0
-    @State var offsetY: CGFloat = 0
+    @Binding var offsetX: CGFloat
+    @Binding var offsetY: CGFloat
     var smallWidth = 198
     var body: some View {
         RoundedRectangle(cornerRadius: 48)
@@ -201,105 +246,16 @@ struct loadOrPickImage: View {
             .overlay {
                 Image(imageName)
                     .frame(width: 40, height: 40)
+                    RoundedRectangle(cornerRadius: 48)
+                    .stroke(Color.white, lineWidth: 10)
             }
             .onAppear {
                 DispatchQueue.main.asyncAfter(deadline: .now() + 4) {
                     Width = 198
-                    offsetX = -80
-                    offsetY = -80
                 }
             }
             .offset(x: offsetX, y: offsetY)
             .animation(.spring())
-    }
-}
-
-struct YourPick: View {
-    var body: some View {
-        VStack {
-            VStack(spacing: 12) {
-                Text("Your pick")
-                    .foregroundColor(.black)
-                    .font(.system(size: 54))
-                    .multilineTextAlignment(.center)
-                    .fontWeight(.bold)
-                
-                Text("Score 0:0")
-                    .foregroundColor(Color(red: 103/255, green: 80/255, blue: 164/255))
-            }
-            
-            Spacer(minLength: 226)
-            
-            VStack {
-                //                choiceButton(image: "scissors")
-            }
-            
-            Spacer()
-            //            numberOfPlayers(players: "I changed my mind")
-        }
-        .padding(.top, 120)
-        .padding(.bottom, 40)
-        .ignoresSafeArea()
-    }
-}
-
-struct opponentPick: View {
-    var body: some View {
-        VStack {
-            VStack(spacing: 136) {
-                Text("Your opponent’s pick")
-                    .foregroundColor(.black)
-                    .font(.system(size: 54))
-                    .multilineTextAlignment(.center)
-                    .fontWeight(.bold)
-                
-//                                choiceButton(isSelected: false, image: "scissors")
-            }
-            
-            Spacer()
-        }
-        .padding(.top, 120)
-        .ignoresSafeArea()
-    }
-}
-
-struct Result: View {
-    var title: String
-    var color: [Color]
-    var body: some View {
-        VStack {
-            VStack(spacing: 12) {
-                Text(title)
-                    .gradientForeground(colors: color)
-                    .font(.system(size: 54))
-                    .multilineTextAlignment(.center)
-                    .fontWeight(.bold)
-                
-                Text("Score 0:0")
-                    .foregroundColor(Color(red: 103/255, green: 80/255, blue: 164/255))
-                
-                HStack {
-                    RoundedRectangle(cornerRadius: 48)
-                        .frame(width: 198, height: 128)
-                        .foregroundColor(Color(red: 243/255, green: 242/255, blue: 248/255))
-                    
-                        .overlay {
-                            Image("scissors")
-                                .frame(width: 80, height: 80)
-                        }
-                    
-                    RoundedRectangle(cornerRadius: 48)
-                        .frame(width: 190, height: 128)
-                        .foregroundColor(Color(red: 243/255, green: 242/255, blue: 248/255))
-                    
-                        .overlay {
-                            Image("rock")
-                                .frame(width: 80, height: 80)
-                        }
-                        .offset(x: -60)
-                }
-            }
-        }
     }
 }
 
@@ -378,5 +334,108 @@ struct changeOrNext: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+struct Result: View {
+    var title: String
+    var color: [Color]
+    var body: some View {
+        VStack {
+            VStack(spacing: 12) {
+                Text(title)
+                    .gradientForeground(colors: color)
+                    .font(.system(size: 54))
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.bold)
+                
+                Text("Score 0:0")
+                    .foregroundColor(Color(red: 103/255, green: 80/255, blue: 164/255))
+                
+                HStack {
+                    RoundedRectangle(cornerRadius: 48)
+                        .frame(width: 198, height: 128)
+                        .foregroundColor(Color(red: 243/255, green: 242/255, blue: 248/255))
+                    
+                        .overlay {
+                            Image("scissors")
+                                .frame(width: 80, height: 80)
+                        }
+                    
+                    RoundedRectangle(cornerRadius: 48)
+                        .frame(width: 190, height: 128)
+                        .foregroundColor(Color(red: 243/255, green: 242/255, blue: 248/255))
+                    
+                        .overlay {
+                            Image("rock")
+                                .frame(width: 80, height: 80)
+                        }
+                        .offset(x: -60)
+                }
+            }
+        }
+    }
+}
+
+
+struct opponentPick: View {
+    var body: some View {
+        VStack {
+            VStack(spacing: 136) {
+                Text("Your opponent’s pick")
+                    .foregroundColor(.black)
+                    .font(.system(size: 54))
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.bold)
+                
+//                                choiceButton(isSelected: false, image: "scissors")
+            }
+            
+            Spacer()
+        }
+        .padding(.top, 120)
+        .ignoresSafeArea()
+    }
+}
+
+
+
+struct YourPick: View {
+    var body: some View {
+        VStack {
+            VStack(spacing: 12) {
+                Text("Your pick")
+                    .foregroundColor(.black)
+                    .font(.system(size: 54))
+                    .multilineTextAlignment(.center)
+                    .fontWeight(.bold)
+                
+                Text("Score 0:0")
+                    .foregroundColor(Color(red: 103/255, green: 80/255, blue: 164/255))
+            }
+            
+            Spacer(minLength: 226)
+            
+            VStack {
+                //                choiceButton(image: "scissors")
+            }
+            
+            Spacer()
+            //            numberOfPlayers(players: "I changed my mind")
+        }
+        .padding(.top, 120)
+        .padding(.bottom, 40)
+        .ignoresSafeArea()
     }
 }
---
+++ RockPaperScissorsApp/RockPaperScissorsApp/Model.swift
@@ -0,0 +1,76 @@
+//
+//  Model.swift
+//  RockPaperScissorsApp
+//
+//  Created by Nursat Sakyshev on 16.04.2023.
+//
+
+import Foundation
+
+import UIKit
+
+enum elements: String, CaseIterable {
+    case rock = "rock"
+    case paper = "paper"
+    case scissors = "scissors"
+}
+
+protocol canPlayInRSPGame {
+    var choice: elements { get set }
+    
+    func play(player2: Player)
+}
+
+enum statusGame {
+    case win
+    case lose
+    case tie
+}
+
+class Player: canPlayInRSPGame {
+    
+    var choice: elements
+    var score = 0
+    var name: String
+    var status: statusGame
+    
+    init(name: String, choice: elements) {
+        self.name = name
+        self.choice = choice
+        self.status = .tie
+    }
+    
+    func play(player2: Player) {
+        let first = self.choice
+        let second = player2.choice
+        
+//        print("\(self.name) chose \(first.rawValue), \(player2.name) chose \(second.rawValue)")
+        
+        if first.rawValue == second.rawValue {
+            status = .tie
+//            print("There's no a winner")
+        }
+        else { // win
+            if (self.choice == .rock && player2.choice == .scissors
+                || self.choice == .scissors && player2.choice == .paper
+                || self.choice == .paper && player2.choice == .rock) {
+                
+                status = .win
+//                print("\(first.rawValue) beats \(second.rawValue)")
+//                print("\(self.name) wins")
+            }
+            else { // lose
+//                print("\(second.rawValue) beats \(first.rawValue)")
+//                print("\(player2.name) wins")
+                status = .lose
+            }
+        }
+    }
+}
+
+let allCases = elements.allCases
+
+var p1 = Player(name: "Nursat", choice: allCases[Int.random(in: 0..<allCases.count)])
+var p2 = Player(name: "Madi", choice: allCases[Int.random(in: 0..<allCases.count)])
+
+
